427a789d3403aa4db24ab2f5e00db96415c2e21d
JavierM42 <Javier Morales>
2021-06-12 10:37:20 -0300

    Add more text

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center max-w-3xl mx-auto text-gray-500">
+      <main className="flex flex-col items-center max-w-3xl mx-auto text-2xl text-gray-500">
         {/* TODO change for image when we have src */}
         <div className="mt-24 mb-8 shadow-lg rounded-xl w-72 h-72" />
         <h1 className="mb-20 text-5xl text-center">Hi, Javi here.</h1>
@@ -31,22 +31,54 @@ export default function Home() {
           .
         </h2>
 
-        <p className="mb-24 text-2xl text-center">
+        <p className="mb-24 text-center">
           I love to build things that work and look good while doing so.
         </p>
 
-        <p className="text-2xl">
-          I started playing around with ActionScript when I was twelve. I{' '}
-          <em className="italic">loved</em>{' '}
-          it. I'm now twenty-five and play around with{' '}
-          <TextSwitcher texts={[
-            'Javascript',
-            'React and Vue',
-            'Ruby on Rails',
-            'Elixir'
-          ]} />{' '}
-          for a living. And I still love it.
-        </p>
+        <div>
+          <p>
+            I started playing around with ActionScript when I was twelve. I{' '}
+            <em className="italic">loved</em>{' '}
+            it. I'm now twenty-five and play around with{' '}
+            <TextSwitcher texts={[
+              'Javascript',
+              'React and Vue',
+              'Ruby on Rails',
+              'Elixir'
+            ]} />{' '}
+            for a living. And I still love it.
+          </p>
+
+          <p>
+            I'm currently wroking as a software engineer for{' '}
+            <TextSwitcher texts={['an innovative online school']} />.
+          </p>
+
+          <p>
+            Before that,{' '}
+            <TextSwitcher texts={[
+              'I helped build an app for remote agile retrospectives',
+              'I was project lead in an EdTech company',
+              'I built a website to help my local Magic: The Gathering community',
+              'I did my thesis on near-symmetrical graph drawing',
+            ]} />.
+          </p>
+
+          <p>
+            In my spare time I{' '}
+            <TextSwitcher texts={[
+              'play piano and guitar', // somehow link YouTube covers?
+              "solve Rubik's cubes", // Definitely not a pro, but slowly getting my solve times lower.
+              'play Age of Empires II', // and sometimes other games, but mostly Age of Empires II.
+              'play boardgames',
+              'travel as much as I can'
+            ]} />.
+          </p>
+
+          <p>
+            Drop me a line at hello@javierm42.dev.
+          </p>
+        </div>
       </main>
     </>
   )
