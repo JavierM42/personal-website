3fd6f40d633cea76c17698f91c408f5d35e39f6e
JavierM42 <javier@wyeworks.com>
2022-10-10 14:01:53 -0300

    smaller blog post text

--- components/BlogPostCard.tsx
+++ components/BlogPostCard.tsx
@@ -10,7 +10,7 @@ const BlogPostCard: FC<Props> = ({ title, summary, url }: Props) => {
   return (
     <li className="px-4 py-2 shadow bg-surface">
       <div className="font-bold text-md">{title}</div>
-      {summary}
+      <p className="text-sm text-outline">{summary}</p>
       <div className="flex justify-end">
         <a href={url} target="_blank" rel="noreferrer">
           Read the post
