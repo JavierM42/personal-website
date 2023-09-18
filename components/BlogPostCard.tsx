be20c3cec8200c4b158874d49319e4ab7955f450
JavierM42 <javier@wyeworks.com>
2023-09-18 13:21:25 -0300

    adjust shadow

--- components/BlogPostCard.tsx
+++ components/BlogPostCard.tsx
@@ -16,7 +16,7 @@ const BlogPostCard: FC<Props> = ({
   url,
 }: Props) => {
   return (
-    <li className="[&>*]:whitespace-normal rounded-xl py-2 snap-center px-4 shadow-xl shadow-primary-container/50 space-y-6 first:ml-0 last:mr-0 mx-1.5 sm:mx-4 bg-surface w-[372px] max-w-[calc(100vw-42px)] flex-shrink-0">
+    <li className="[&>*]:whitespace-normal rounded-xl py-2 snap-center px-4 shadow-xl shadow-primary-container/40 space-y-6 first:ml-0 last:mr-0 mx-1.5 sm:mx-4 bg-surface w-[372px] max-w-[calc(100vw-42px)] flex-shrink-0">
       <div className="w-48 h-48 mx-auto">{illustration}</div>
       <h3 className="w-full mt-2 text-lg font-bold">{title}</h3>
       <p className="w-full mb-4 text-sm text-outline">{summary}</p>
