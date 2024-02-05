f39488f822db3fa86d7d6f5fe62773ae6a550af0
JavierM42 <javier@wyeworks.com>
2024-02-05 14:03:17 -0300

    Paragraph classname, slide ref

--- components/blog/dvd/Paragraph.tsx
+++ components/blog/dvd/Paragraph.tsx
@@ -4,12 +4,20 @@ import { ReactNode } from "react";
 const Paragraph = ({
   children,
   inCode,
+  className,
 }: {
   children: ReactNode;
   inCode?: boolean;
+  className?: string;
 }) => {
   return (
-    <p className={classNames("w-full max-w-lg text-justify", inCode && "my-1")}>
+    <p
+      className={classNames(
+        "w-full max-w-lg text-justify",
+        inCode && "my-1",
+        className
+      )}
+    >
       {children}
     </p>
   );
