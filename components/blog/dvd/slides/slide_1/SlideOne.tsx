14a245202b5356df68af3e374f3be1e939dad32c
JavierM42 <javier@wyeworks.com>
2024-02-06 14:42:26 -0300

    responsivize slide 1

--- components/blog/dvd/slides/slide_1/SlideOne.tsx
+++ components/blog/dvd/slides/slide_1/SlideOne.tsx
@@ -7,18 +7,18 @@ export default function SlideOne() {
   return (
     <Slide
       className={classNames(
-        "relative overflow-clip bg-surface/40",
+        "relative overflow-clip bg-surface/40 max-w-[80vw] md:max-w-auto aspect-[4/3] mx-auto py-0",
         crtStyles.crt
       )}
       transparent
       noPadding
     >
       <DvdScreensaver fluidSize />
-      <div className="absolute inset-x-0 flex flex-col items-center gap-6 bottom-[20%] px-4">
-        <h2 className="text-4xl italic normal-case text-on-surface">
+      <div className="absolute inset-x-0 flex flex-col md:items-center gap-2 md:gap-6 bottom-[20%] px-4">
+        <h2 className="text-xl italic normal-case md:text-4xl text-on-surface">
           CSS-only DVD Screensaver animation
         </h2>
-        <h3 className="text-2xl tracking-widest text-on-surface">
+        <h3 className="tracking-widest text-md md:text-2xl text-on-surface">
           An interactive walkthrough
         </h3>
       </div>
