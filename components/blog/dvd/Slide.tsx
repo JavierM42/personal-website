45d6b55db7303934949c9bcbc872d952379b72b9
JavierM42 <javier@wyeworks.com>
2024-02-07 10:34:44 -0300

    FIX responsiveness

--- components/blog/dvd/Slide.tsx
+++ components/blog/dvd/Slide.tsx
@@ -13,15 +13,15 @@ const Slide = forwardRef<HTMLDivElement, Props>(
     return (
       <div
         className={classNames(
-          "flex items-center w-full min-h-screen snap-center bg-surface/40 md:bg-transparent"
+          "flex items-center w-full min-h-screen snap-center bg-surface/40 md:bg-transparent pt-16 pb-2"
         )}
         ref={ref}
       >
         <div
           className={classNames(
-            "w-full md:shadow-xl md:aspect-[4/3] md:rounded-xl py-20",
+            "w-full md:shadow-xl md:aspect-[4/3] md:rounded-xl py-20 max-h-[calc(100vh-72px)]",
             !transparent && "md:bg-surface/40",
-            !noPadding && "px-8 md:py-4",
+            !noPadding && "px-8 md:py-6",
             className
           )}
         >
